cc1321be31be3c778e76da6e09704e62d030f9dc
Firstname Lastname <your_email@youremail.com>
2023-05-06 15:37:21 +0530

    Blogs1

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -10,7 +10,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
+    private String username;
     private String password;
     private String firstName;
     private String lastName;
@@ -18,16 +18,17 @@ public class User {
     @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
     private List<Blog> blogList=new ArrayList<>();
 
-    public User() {
-    }
-
-    public User(int id, String name, String password, String firstName, String lastName, List<Blog> blogList) {
+    public User(int id, String username, String password, String firstName, String lastName, List<Blog> blogList) {
         this.id = id;
-        this.name = name;
+        this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
         this.blogList = blogList;
+    }
+
+    public User() {
+
     }
 
     public int getId() {
@@ -38,12 +39,12 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
